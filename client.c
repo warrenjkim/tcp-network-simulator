1af7051f5d7098251a2a37106d0df53bda2b859e
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2023-12-06 14:12:02 -0800

    rewrote project

--- client.c
+++ client.c
@@ -8,185 +8,237 @@
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
-  int listen_sockfd, send_sockfd;
-  struct sockaddr_in client_addr, server_addr_to, server_addr_from;
-  socklen_t addr_size = sizeof(server_addr_to);
-  struct timeval tv;
+    int listen_sockfd, send_sockfd;
+    struct sockaddr_in client_addr, server_addr_to, server_addr_from;
+    socklen_t addr_size = sizeof(server_addr_to);
+    struct timeval tv;
 
-  tv.tv_sec = 0;
-  tv.tv_usec = TIMEOUT * 100000;
+    tv.tv_sec = 0;
+    tv.tv_usec = TIMEOUT * 100000;
 
-  // read filename from command line argument
-  if (argc != 2) {
-    printf("Usage: ./client <filename>\n");
-    return 1;
-  }
-  char *filename = argv[1];
+    // read filename from command line argument
+    if (argc != 2) {
+        printf("Usage: ./client <filename>\n");
+        return 1;
+    }
+    char *filename = argv[1];
 
-  // Create a UDP socket for listening
-  listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (listen_sockfd < 0) {
-    perror("Could not create listen socket");
-    return 1;
-  }
+    // Create a UDP socket for listening
+    listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (listen_sockfd < 0) {
+        perror("Could not create listen socket");
+        return 1;
+    }
 
-  // receive timeout
-  if (setsockopt(listen_sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
-    perror("couldn't set receive timeout");
-    return 1;
-  }
+    // receive timeout
+    if (setsockopt(listen_sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) <
+        0) {
+        perror("couldn't set receive timeout");
+        return 1;
+    }
 
-  // Create a UDP socket for sending
-  send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (send_sockfd < 0) {
-    perror("Could not create send socket");
-    return 1;
-  }
+    // Create a UDP socket for sending
+    send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (send_sockfd < 0) {
+        perror("Could not create send socket");
+        return 1;
+    }
 
-  // Configure the server address structure to which we will send data
-  memset(&server_addr_to, 0, sizeof(server_addr_to));
-  server_addr_to.sin_family = AF_INET;
-  server_addr_to.sin_port = htons(SERVER_PORT_TO);
-  server_addr_to.sin_addr.s_addr = inet_addr(SERVER_IP);
+    // Configure the server address structure to which we will send data
+    memset(&server_addr_to, 0, sizeof(server_addr_to));
+    server_addr_to.sin_family = AF_INET;
+    server_addr_to.sin_port = htons(SERVER_PORT_TO);
+    server_addr_to.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-  // Configure the client address structure
-  memset(&client_addr, 0, sizeof(client_addr));
-  client_addr.sin_family = AF_INET;
-  client_addr.sin_port = htons(CLIENT_PORT);
-  client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // Configure the client address structure
+    memset(&client_addr, 0, sizeof(client_addr));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(CLIENT_PORT);
+    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  // Bind the listen socket to the client address
-  if (bind(listen_sockfd, (struct sockaddr *)&client_addr,
-           sizeof(client_addr)) < 0) {
-    perror("Bind failed");
-    close(listen_sockfd);
-    return 1;
-  }
+    // Bind the listen socket to the client address
+    if (bind(listen_sockfd, (struct sockaddr *)&client_addr,
+             sizeof(client_addr)) < 0) {
+        perror("Bind failed");
+        close(listen_sockfd);
+        return 1;
+    }
 
-  // Open file for reading
-  FILE *fp = fopen(filename, "rb");
-  if (fp == NULL) {
-    perror("Error opening file");
+    // Open file for reading
+    FILE *fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        perror("Error opening file");
+        close(listen_sockfd);
+        close(send_sockfd);
+        return 1;
+    }
+
+    // TODO: Read from file, and initiate reliable data transfer to the server
+
+    struct packet pkt;
+    struct packet ack_pkt;
+    char buffer[PAYLOAD_SIZE];
+    unsigned short seq_num = 0;
+    unsigned short ack_num = 0;
+    char last = 0;
+    char ack = 0;
+
+
+    ack_pkt.seqnum = 0;
+    ack_pkt.acknum = 0;
+    ack_pkt.last = 0;
+    ack_pkt.ack = 0;
+
+    State state = SLOW_START;
+
+    // fast retransmit
+    int dup_count = 0;
+
+    // window growth
+    ssize_t mss = 0;
+    size_t ssthresh = (unsigned short)(-1);
+
+    Queue *cwnd = queue_init();
+    ssize_t recv_len = 0;
+
+    struct stat info;
+    if (stat(filename, &info) != 0) {
+        perror("stat error");
+        exit(1);
+    }
+    
+    char *file = (char *)malloc(sizeof(char) * info.st_size);
+    if (!file) {
+        perror("malloc error");
+        exit(1);
+    }
+
+    if (fread(file, sizeof(char), info.st_size, fp) != (unsigned long) info.st_size) {
+        perror("fread error");        
+        exit(1);
+    }
+
+    int i = -1;
+
+    while (1) {
+        mss = queue_pop(cwnd, ack_pkt.acknum);
+
+        if (mss == 0)
+            dup_count++;
+
+        while (0 < mss--) {
+            switch (state) {
+            case SLOW_START:
+                cwnd->max_size += MSS;
+                if (ssthresh < cwnd->max_size)
+                    state = CONGESTION_AVOIDANCE;
+                break;
+            case CONGESTION_AVOIDANCE:
+                cwnd->max_size += (double)(MSS) / (size_t)(cwnd->max_size);
+                break;
+            case FAST_RECOVERY:
+                cwnd->max_size = ssthresh;
+                state = SLOW_START;
+
+                // tv.tv_sec = 0;
+                // tv.tv_usec = TIMEOUT * 100000;
+
+                // if (setsockopt(listen_sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) <
+                //         0) {
+                //     perror("couldn't set receive timeout");
+                //     return 1;
+                // }
+                break;
+            }
+            dup_count = 0;
+        }
+
+        if (state == FAST_RECOVERY && 3 < dup_count)
+            cwnd->max_size += MSS;
+
+        if (dup_count == 3) {
+            state = FAST_RECOVERY;
+            ssthresh = (size_t)(cwnd->max_size) / 2 < 2
+                           ? 2
+                           : (size_t)(cwnd->max_size) / 2;
+            cwnd->max_size = ssthresh + 3;
+            struct packet *resend = queue_top(cwnd);
+            sendto(send_sockfd, resend, sizeof(struct packet), MSG_CONFIRM,
+                   (const struct sockaddr *)&server_addr_to, addr_size);
+
+            printf("fast retransmit: ");
+            print_send(resend, 1);
+        }
+
+        if (recv_len < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            ssthresh = (size_t)(cwnd->max_size) / 2 < 2
+                           ? 2
+                           : (size_t)(cwnd->max_size) / 2;
+            cwnd->max_size = MSS;
+            state = SLOW_START;
+
+            struct packet *resend = queue_top(cwnd);
+            sendto(send_sockfd, resend, sizeof(struct packet), MSG_CONFIRM,
+                   (const struct sockaddr *)&server_addr_to, addr_size);
+
+            // if (1000000 <= (tv.tv_usec *= 2)) {
+            //     tv.tv_sec += tv.tv_usec / 1000000;
+            //     tv.tv_usec %= 1000000;
+            // }
+
+            // if (setsockopt(listen_sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) <
+            //         0) {
+            //     perror("couldn't set receive timeout");
+            //     return 1;
+            // }
+
+            printf("timeout: ");
+            print_send(resend, 1);
+        }
+
+        // send while cwnd is not full
+        while (!queue_full(cwnd) && !last) {
+            off_t bytes_read = info.st_size - (++i * PAYLOAD_SIZE);
+            if (bytes_read < PAYLOAD_SIZE) {
+                memcpy(buffer, file + (i * PAYLOAD_SIZE), bytes_read);
+                last = 1;
+            }
+            else {
+                bytes_read = PAYLOAD_SIZE;
+                memcpy(buffer, file + (i * PAYLOAD_SIZE), PAYLOAD_SIZE);
+            }
+            /* bytes_read = fread(buffer, sizeof(char), PAYLOAD_SIZE, fp); */
+
+            /* if (bytes_read < PAYLOAD_SIZE) */
+            /*     last = 1; */
+
+            build_packet(&pkt, seq_num, ack_num, last, ack, bytes_read, buffer);
+            sendto(send_sockfd, &pkt, sizeof(struct packet), MSG_CONFIRM,
+                   (const struct sockaddr *)&server_addr_to, addr_size);
+
+            printf("client: ");
+            print_send(&pkt, 0);
+
+            seq_num++;
+            ack_num++;
+
+            cwnd = queue_push(cwnd, &pkt);
+        }
+
+        recv_len = recvfrom(listen_sockfd, &ack_pkt, sizeof(ack_pkt), 0,
+                            (struct sockaddr *)&server_addr_from, &addr_size);
+
+        printf("client: ");
+        print_recv(&ack_pkt);
+
+        if (ack_pkt.last && queue_top(cwnd)->seqnum <= ack_pkt.acknum)
+            break;
+
+        printf("\n");
+    }
+
+    fclose(fp);
     close(listen_sockfd);
     close(send_sockfd);
-    return 1;
-  }
-
-  // TODO: Read from file, and initiate reliable data transfer to the server
-
-  struct packet pkt;
-  struct packet ack_pkt;
-  char buffer[PAYLOAD_SIZE];
-  unsigned short seq_num = 0;
-  unsigned short ack_num = 0;
-  char last = 0;
-  char ack = 0;
-
-  size_t bytes_read = 0;
-
-  ack_pkt.seqnum = (unsigned short)(-1);
-  ack_pkt.acknum = (unsigned short)(-1);
-
-  State state = SLOW_START;
-
-  // fast retransmit
-  int count = 0;
-  struct packet dup_pkt;
-
-  // window growth
-  size_t mss = 0;
-  size_t ssthresh = (unsigned short)(-1);
-
-  Queue *cwnd = queue_init();
-
-  ssize_t timeout = 0;
-
-  while (1) {
-    if (ack_pkt.last)
-      break;
-
-    if (ack_pkt.acknum != (unsigned short)(-1)) {
-      if (dup_pkt.acknum == ack_pkt.acknum) {
-        count += mss;
-      } else {
-        mss = queue_pop(cwnd, ack_pkt.acknum);
-        dup_pkt = ack_pkt;
-        count = 0;
-      }
-    }
-
-    if (state == SLOW_START) {
-      cwnd->max_size += mss;
-
-      if (ssthresh < cwnd->max_size)
-        state = CONGESTION_AVOIDANCE;
-    }
-
-    else if (state == CONGESTION_AVOIDANCE) {
-      cwnd->max_size += ((double)(MSS) / (size_t)(cwnd->max_size));
-    }
-
-    else if (state == FAST_RECOVERY) {
-      if (dup_pkt.seqnum != ack_pkt.seqnum ||
-          dup_pkt.acknum != ack_pkt.acknum) {
-        cwnd->max_size = ssthresh;
-        state = CONGESTION_AVOIDANCE;
-      } else
-        cwnd->max_size += MSS;
-    }
-
-    if (count == 3) {
-      ssthresh = ((size_t)(cwnd->max_size / 2) < (2 * MSS))
-                     ? (2 * MSS)
-                     : (size_t)(cwnd->max_size / 2);
-      cwnd->max_size = ssthresh + (3 * MSS);
-      state = FAST_RECOVERY;
-      count = 0;
-
-      struct packet *resend = queue_get(cwnd, dup_pkt.acknum);
-
-      sendto(send_sockfd, resend, sizeof(*resend), MSG_CONFIRM,
-             (const struct sockaddr *)&server_addr_to, addr_size);
-    }
-
-    if (timeout < 0) {
-      ssthresh =
-          ((size_t)(cwnd->max_size / 2) < 2) ? 2 : (size_t)(cwnd->max_size / 2);
-      cwnd->max_size = 1 * MSS;
-
-      state = SLOW_START;
-
-      struct packet *resend = queue_top(cwnd);
-      sendto(send_sockfd, resend, sizeof(*resend), MSG_CONFIRM,
-             (const struct sockaddr *)&server_addr_to, addr_size);
-    }
-
-    while (!queue_full(cwnd) && !last) {
-      bytes_read = fread(buffer, sizeof(char), PAYLOAD_SIZE, fp);
-
-      if (bytes_read < PAYLOAD_SIZE)
-        last = 1;
-
-      build_packet(&pkt, seq_num, ack_num, last, ack, bytes_read, buffer);
-      seq_num += 1;
-      ack_num += 1;
-
-      sendto(send_sockfd, &pkt, sizeof(pkt), MSG_CONFIRM,
-             (const struct sockaddr *)&server_addr_to, addr_size);
-
-      cwnd = queue_push(cwnd, &pkt);
-      // usleep(5);
-    }
-
-    timeout = recvfrom(listen_sockfd, &ack_pkt, sizeof(ack_pkt), MSG_WAITALL,
-                       (struct sockaddr *)&server_addr_from, &addr_size);
-
-    if (ack_pkt.last)
-      break;
-  }
-
-  fclose(fp);
-  close(listen_sockfd);
-  close(send_sockfd);
-  return 0;
+    return 0;
 }
--- server.c
+++ server.c
@@ -7,98 +7,156 @@
 #include "utils.h"
 
 int main() {
-  int listen_sockfd, send_sockfd;
-  struct sockaddr_in server_addr, client_addr_from, client_addr_to;
-  struct packet buffer;
-  socklen_t addr_size = sizeof(client_addr_from);
+    int listen_sockfd, send_sockfd;
+    struct sockaddr_in server_addr, client_addr_from, client_addr_to;
+    socklen_t addr_size = sizeof(client_addr_from);
 
-  // Create a UDP socket for sending
-  send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (send_sockfd < 0) {
-    perror("Could not create send socket");
-    return 1;
-  }
-
-  // Create a UDP socket for listening
-  listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (listen_sockfd < 0) {
-    perror("Could not create listen socket");
-    return 1;
-  }
-
-  // Configure the server address structure
-  memset(&server_addr, 0, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(SERVER_PORT);
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-  // Bind the listen socket to the server address
-  if (bind(listen_sockfd, (struct sockaddr *)&server_addr,
-           sizeof(server_addr)) < 0) {
-    perror("Bind failed");
-    close(listen_sockfd);
-    return 1;
-  }
-
-  // Configure the client address structure to which we will send ACKs
-  memset(&client_addr_to, 0, sizeof(client_addr_to));
-  client_addr_to.sin_family = AF_INET;
-  client_addr_to.sin_addr.s_addr = inet_addr(LOCAL_HOST);
-  client_addr_to.sin_port = htons(CLIENT_PORT_TO);
-
-  // Open the target file for writing (always write to output.txt)
-  FILE *fp = fopen("output.txt", "wb");
-
-  // TODO: Receive file from the client and save it as output.txt
-
-  unsigned short expected_seq_num = 0;
-  int recv_len;
-  struct packet ack_pkt;
-
-  unsigned short seq_num = 0;
-  unsigned short ack_num = 0;
-  unsigned short last_seqnum = -1;
-  char last = 0;
-  char ack = 1;
-
-  Heap *heap = heap_init();
-
-  while (1) {
-    recv_len = recvfrom(listen_sockfd, &buffer, sizeof(buffer), MSG_WAITALL,
-                        (struct sockaddr *)&client_addr_from, &addr_size);
-
-    if (buffer.last)
-      last_seqnum = buffer.seqnum;
-
-    seq_num = buffer.seqnum < seq_num ? seq_num : buffer.seqnum;
-
-    if (expected_seq_num <= buffer.seqnum)
-      heap = heap_push(heap, &buffer);
-
-    struct packet min_pkt = heap_top(heap);
-
-    while (0 < heap->size && (expected_seq_num == min_pkt.seqnum)) {
-      expected_seq_num++;
-      fwrite(min_pkt.payload, sizeof(char), min_pkt.length, fp);
-      heap = heap_pop(heap);
-      min_pkt = heap_top(heap);
+    // Create a UDP socket for sending
+    send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (send_sockfd < 0) {
+        perror("Could not create send socket");
+        return 1;
     }
 
-    ack_num = expected_seq_num;
+    // Create a UDP socket for listening
+    listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (listen_sockfd < 0) {
+        perror("Could not create listen socket");
+        return 1;
+    }
 
-    if (last_seqnum <= expected_seq_num)
-      last = 1;
+    // Configure the server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    // Bind the listen socket to the server address
+    if (bind(listen_sockfd, (struct sockaddr *)&server_addr,
+             sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(listen_sockfd);
+        return 1;
+    }
+
+    // Configure the client address structure to which we will send ACKs
+    memset(&client_addr_to, 0, sizeof(client_addr_to));
+    client_addr_to.sin_family = AF_INET;
+    client_addr_to.sin_addr.s_addr = inet_addr(LOCAL_HOST);
+    client_addr_to.sin_port = htons(CLIENT_PORT_TO);
+
+    // Open the target file for writing (always write to output.txt)
+    FILE *fp = fopen("output.txt", "wb");
+
+    // TODO: Receive file from the client and save it as output.txt
+
+    unsigned short expected_seq_num = 0;
+    struct packet ack_pkt;
+    struct packet buffer;
+
+    unsigned short seq_num = 0;
+    unsigned short ack_num = 0;
+    char last = 0;
+    char ack = 1;
+
+    Heap *heap = heap_init();
+
+    ssize_t recv_len = 0;
+    struct packet min_pkt;
+
+    /* char *to_write = (char *)(malloc(sizeof(char) * PAYLOAD_SIZE * SCALE)); */
+    int i = 0;
+    size_t write_bytes = 0;
+
+    char buf[PAYLOAD_SIZE * SCALE];
+
+    while (1) {
+        // read
+        recv_len =
+            recvfrom(listen_sockfd, &buffer, sizeof(buffer), MSG_DONTWAIT,
+                     (struct sockaddr *)&client_addr_from, &addr_size);
+
+        if (recv_len < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            if (write_bytes) {
+                fwrite(buf, sizeof(char), write_bytes, fp);
+                i = 0;
+                write_bytes = 0;
+                /* fwrite(to_write, sizeof(char), write_bytes, fp); */
+                /* i = 0; */
+
+                /* if (!buffer.last) */
+                /*     write_bytes = 0; */
+            }
+            continue;
+        }
+
+        printf("server: ");
+        print_recv(&buffer);
+
+        if (expected_seq_num <= buffer.seqnum)
+            heap = heap_push(heap, &buffer);
+
+        // process packets in order
+        min_pkt = heap_top(heap);
+        while (!heap_empty(heap) && (expected_seq_num == min_pkt.seqnum)) {
+            expected_seq_num++;
+
+            memcpy(buf + (i++ * PAYLOAD_SIZE), min_pkt.payload, min_pkt.length);
+            write_bytes += min_pkt.length;
+
+            if (i == SCALE) {
+                fwrite(buf, sizeof(char), write_bytes, fp);
+                i = 0;
+                write_bytes = 0;
+            }
+            /* memcpy(to_write + (i++ * PAYLOAD_SIZE), min_pkt.payload, */
+            /*        min_pkt.length); */
+            /* write_bytes += min_pkt.length; */
+            /* if (i == SCALE) { */
+            /*     i = 0; */
+            /*     if (!realloc(to_write, write_bytes + (PAYLOAD_SIZE * SCALE))) { */
+            /*         perror("realloc failed"); */
+            /*         exit(1); */
+            /*     } */
+            /* } */
+
+            heap = heap_pop(heap);
+            min_pkt = heap_top(heap);
+        }
+
+        seq_num = buffer.seqnum;
+        ack_num = expected_seq_num;
+        build_packet(&ack_pkt, seq_num, ack_num, last, ack, 0, "");
+
+        sendto(send_sockfd, &ack_pkt, sizeof(ack_pkt), MSG_CONFIRM,
+               (const struct sockaddr *)&client_addr_to, addr_size);
+
+        printf("server: ");
+        print_send(&ack_pkt, 0);
+
+        if (buffer.last && buffer.seqnum <= expected_seq_num) {
+            if (write_bytes) {
+                fwrite(buf, sizeof(char), write_bytes, fp);
+                i = 0;
+                write_bytes = 0;
+            }
+            /* if (write_bytes) */
+            /*     fwrite(to_write, sizeof(char), write_bytes, fp); */
+            break;
+        }
+
+        printf("\n");
+    }
+
+    last = 1;
     build_packet(&ack_pkt, seq_num, ack_num, last, ack, 0, "");
     sendto(send_sockfd, &ack_pkt, sizeof(ack_pkt), MSG_CONFIRM,
            (const struct sockaddr *)&client_addr_to, addr_size);
 
-    if (last)
-      break;
-  }
-
-  fclose(fp);
-  close(listen_sockfd);
-  close(send_sockfd);
-  return 0;
+    heap_destroy(heap);
+    // free(to_write);
+    fclose(fp);
+    close(listen_sockfd);
+    close(send_sockfd);
+    return 0;
 }
